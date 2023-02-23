ff0b8d8d7ae5737144100feb27ace46578ed90ae
Ian <yyh_lee@fitpet.co.kr>
2023-02-23 15:49:21 +0900

    ✨ .prettier setting
    - prettier 설치 및 .prettierrc.json 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
