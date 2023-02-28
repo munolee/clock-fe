6d9db28893d06525062c0f6f952bb7aece751247
Ian <yyh_lee@fitpet.co.kr>
2023-02-28 16:47:17 +0900

    🩹 path alias 수정
    - tsconfig 수정 -vite-tsconfig-paths 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 8000,
-  },
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      {
-        find: '@styles',
-        replacement: path.resolve(__dirname, 'src/styles'),
-      },
-      {
-        find: '@constants',
-        replacement: path.resolve(__dirname, 'src/constants'),
-      },
-      {
-        find: '@pages',
-        replacement: path.resolve(__dirname, 'src/pages'),
-      },
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, 'src/components'),
-      },
-      {
-        find: '@store',
-        replacement: path.resolve(__dirname, 'src/store'),
-      },
-      {
-        find: '@hooks',
-        replacement: path.resolve(__dirname, 'src/hooks'),
-      },
-    ],
   },
 });
